3a67cb735fe82a4bb06a9d5cc1b8b56b86f70937
rnori-harv <rnori@college.harvard.edu>
2023-07-10 10:29:18 -0700

    switched model for deploy

--- src/feed.py
+++ src/feed.py
@@ -212,7 +212,8 @@ def llm_agent():
     
     
     
-    model = ChatOpenAI(temperature=0, model = "gpt-4")
+    # model = ChatOpenAI(temperature=0, model = "gpt-4")
+    model = ChatOpenAI(temperature=0, model = "gpt-3.5-turbo-16k")
 
     planner = load_chat_planner(model)
 
